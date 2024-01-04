b454b2df0df01dcd038059f111e1b585cf15ef08
Andy Albaugh <andypsu5155@gmail.com>
2024-01-04 02:02:41 -0500

    Finished category pages! 3:22:11

--- components/movie-video.tsx
+++ components/movie-video.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import prisma from '@/app/utils/db'
-import { Button } from './ui/button';
+import MovieButtons from './movie-buttons';
 
 async function getData() {
     const data = await prisma.movie.findFirst({
@@ -13,6 +13,7 @@ async function getData() {
             duration: true,
             id: true,
             age: true,
+            youtubeString: true,
         }
     })
 
@@ -39,8 +40,16 @@ export default async function MovieVideo() {
                 <p className='text-white text-lg mt-5 line-clamp-3'>{data?.overview}</p>
 
                 <div className='flex gap-x-3 mt-4'>
-                    <Button>See More</Button>
-                    <Button>Learn More</Button>
+                    <MovieButtons 
+                        age={data?.age as number} 
+                        duration={data?.duration as number} 
+                        id={data?.id as number} 
+                        overview={data?.overview as string}
+                        releaseDate={data?.release as number}
+                        title={data?.title as string}
+                        youtubeUrl={data?.youtubeString as string}
+                        key={data?.id}
+                    />
                 </div>
             </div>
         </div>
--- components/recently-added.tsx
+++ components/recently-added.tsx
@@ -1,5 +1,6 @@
 import prisma from "@/app/utils/db"
 import Image from "next/image";
+import MovieCard from "./movie-card";
 
 async function getData() {
     const data = await prisma.movie.findMany({
@@ -9,7 +10,10 @@ async function getData() {
             title: true,
             WatchLists: true,
             imageString: true,
-            videoSource: true,
+            youtubeString: true,
+            age: true,
+            release: true,
+            duration: true,
         },
         orderBy: {
             createdAt: "desc"
@@ -25,17 +29,42 @@ export default async function RecentlyAdded() {
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 gap-6">
-            {data.map((movie) => (
-                <div key={movie.id} className="relative h-48">
-                    <Image 
-                        src={movie.imageString}
-                        alt="Movie"
-                        className="rounded-sm absolute w-full"
-                        width={500}
-                        height={400}
-                    />
-                </div>
-            ))}
-        </div>
+        {data.map((movie) => (
+          <div key={movie.id} className="relative h-48">
+            <Image
+              src={movie.imageString}
+              alt="Movie"
+              width={500}
+              height={400}
+              className="rounded-sm absolute w-full h-full object-cover"
+            />
+  
+            <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-125 opacity-0 hover:opacity-100">
+              <div className="bg-gradient-to-b from-transparent via-black/50 to-black z-10 w-full h-full rounded-lg flex items-center justify-center border">
+                <Image
+                  src={movie.imageString}
+                  alt="Movie"
+                  width={800}
+                  height={800}
+                  className="absolute w-full h-full -z-10 rounded-lg object-cover"
+                />
+  
+                <MovieCard
+                  movieId={movie.id}
+                  overview={movie.overview}
+                  title={movie.title}
+                  watchListId={movie.WatchLists[0]?.id}
+                  youtubeUrl={movie.youtubeString}
+                  watchList={movie.WatchLists.length > 0 ? true : false}
+                  key={movie.id}
+                  age={movie.age}
+                  time={movie.duration}
+                  year={movie.release}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     )
 }
